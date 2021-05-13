e66712aa7aaf7ea2a5cab560e60779ad41f9e651
Abishek Baskaran <babishek@gmail.com>
2021-05-13 21:42:12 +0530

    wip

--- api-utils/get-available-covid-jabs.js
+++ api-utils/get-available-covid-jabs.js
@@ -1,4 +1,4 @@
-const got = require('got');
+const fetch = require('node-fetch');
 
 
 module.exports = async function(districtId, dateString, vaccineName) {
@@ -6,16 +6,9 @@ module.exports = async function(districtId, dateString, vaccineName) {
   let requestUrl = `https://cdn-api.co-vin.in/api/v2/appointment/sessions/public/findByDistrict?district_id=${districtId}&date=${dateString}&vaccine=${vaccineName}`;
   console.log('Calling API using Got lib: ', requestUrl);
   try{
-    let {body} = await got.get(requestUrl, {
-      responseType: 'json',
-      headers: {
-        'User-Agent': 'PostmanRuntime/7.26.8',
-        'Cache-Control': 'no-cache',
-        'Origin': 'https://apisetu.gov.in',
-        'Accept': 'application/json'
-      }
-    });
-    if(body && body.sessions) {
+    let response = await fetch(requestUrl);
+    let data = await response.json();
+    if(data && data.sessions) {
       output = [];
       for(let session of body.sessions) {
         if (session.available_capacity > 0) {
