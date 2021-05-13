1cda18413c4fd1e67523324aaa10198636c7a156
Abishek Baskaran <babishek@gmail.com>
2021-05-13 22:17:03 +0530

    switched to super-agent

--- api-utils/get-available-covid-jabs.js
+++ api-utils/get-available-covid-jabs.js
@@ -1,10 +1,12 @@
-const fetch = require('node-fetch');
+//const fetch = require('node-fetch');
+const superagent = require('superagent');
 
 
 module.exports = async function(districtId, dateString, vaccineName) {
   let output;
   let requestUrl = `https://cdn-api.co-vin.in/api/v2/appointment/sessions/public/findByDistrict?district_id=${districtId}&date=${dateString}&vaccine=${vaccineName}`;
   console.log('Calling API using Got lib: ', requestUrl);
+  /*
   try{
     let response = await fetch(requestUrl, {headers: {'User-Agent': 'PostmanRuntime/7.26.8'}});
     let data = await response.json();
@@ -18,6 +20,21 @@ module.exports = async function(districtId, dateString, vaccineName) {
     }
   } catch(error) {
     console.log('Could not get data from Cowin API: ', error);
+  }
+  */
+  try {
+    const response = await superagent.get(requestUrl);
+    console.log('Cowin API Response: ', response.body);
+    if(response.body && response.body.sessions) {
+      output = [];
+      for(let session of response.body.sessions) {
+        if (session.available_capacity > 0) {
+          output.push({name: session.name, address: session.address + ', ' + session.pincode, availableCapacity: session.available_capacity});
+        }
+      }
+    }
+  } catch (error) {
+    console.log(error.response.body);
   }
   return output;
 }
